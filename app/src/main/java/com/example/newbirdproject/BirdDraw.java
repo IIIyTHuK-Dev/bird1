7994323e30dfe14a3804951ec151ab7c5959f283
user <malnik.pc@gmail.com>
2020-12-21 18:08:03 +0700

    Init commit

--- app/src/main/java/com/example/newbirdproject/BirdDraw.java
+++ app/src/main/java/com/example/newbirdproject/BirdDraw.java
@@ -4,90 +4,129 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.CountDownTimer;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class BirdDraw extends View {
+import androidx.annotation.NonNull;
 
-    class Timer extends CountDownTimer{
-
-        public Timer() {
-            super(Integer.MAX_VALUE, 1000/60);
-        }
-
-        @Override
-        public void onTick(long l) {
-            BirdDraw.this.invalidate();
-        }
-
-        @Override
-        public void onFinish() {
-
-        }
-    }
-
-    Timer timer;
+public class BirdDraw extends SurfaceView implements SurfaceHolder.Callback {
 
     public BirdDraw(Context context) {
         super(context);
-        timer = new Timer();
-        timer.start();
+        getHolder().addCallback(this);
+    }
+
+    public BirdDraw(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        getHolder().addCallback(this);
+    }
+
+    public BirdDraw(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        SetupThread(getContext());
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    GameThread gameThread;
+
+    private void SetupThread(Context context) {
+        gameThread = new GameThread();
+        gameThread.setContext(context);
+        gameThread.setSurfaceHolder(getHolder());
+
+        gameThread.start();
     }
 
 
+}
+
+
+class GameThread extends Thread {
+
+    private SurfaceHolder surfaceHolder;
+    private Context context;
     private float birdX = 0;
     private float birdY = 0;
     private float goalX = 0;
     private float goalY = 0;
+    private boolean running = true;
 
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        Bitmap bird = BitmapFactory.decodeResource(getResources(),R.drawable.bird1);
-        bird = Bitmap.createScaledBitmap(bird, 250,250,false);
-
-        canvas.drawBitmap(bird, birdX,birdY,new Paint());
-
-        if (birdX < goalX)
-            birdX = birdX + 10;
-        else if (birdX>goalX)
-            birdX = birdX -10;
-
-        if (Math.abs(birdX - goalX) <10) {
-            birdX = goalX;
-        }
-
-        if (birdY < goalY)
-            birdY = birdY + 10;
-        else if (birdY>goalY)
-            birdY = birdY - 10;
-
-        if (Math.abs(birdY - goalY) <10) {
-            birdY = goalY;
-        }
-
-        if (bird.getY() + bird.getHeight() > viewHeight) {
-            bird.setY(viewHeight - bird.getHeight());
-            bird.setVy(-bird.getVy());
-        }
-        else if (bird.getY() < 0) {
-            bird.setY(0);
-            bird.setVy(-bird.getVy());
-        }
-
-        
+    public void setSurfaceHolder(SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
     }
 
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void setGoal(float goalX, float goalY) {
+        this.goalX = goalX;
+        this.goalY = goalY;
+    }
+
+    public void stopDraw() {
+        running = false;
+    }
+
+
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    public void run() {
+        Bitmap bird = BitmapFactory.decodeResource(context.getResources(), R.drawable.bird1);
+        bird = Bitmap.createScaledBitmap(bird, 250, 250, false);
 
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            goalX = event.getX();
-            goalY = event.getY();
+
+        while (running) {
+
+            if (!surfaceHolder.getSurface().isValid())
+                continue;
+
+            Canvas canvas = surfaceHolder.lockCanvas();
+
+            if (canvas != null) {
+                canvas.drawColor(Color.RED);
+
+
+                canvas.drawBitmap(bird, birdX, birdY, new Paint());
+
+                if (birdX < goalX)
+                    birdX = birdX + 10;
+                else if (birdX > goalX)
+                    birdX = birdX - 10;
+
+                if (Math.abs(birdX - goalX) < 10) {
+                    birdX = goalX;
+                }
+
+                if (birdY < goalY)
+                    birdY = birdY + 10;
+                else if (birdY > goalY)
+                    birdY = birdY - 10;
+
+                if (Math.abs(birdY - goalY) < 10) {
+                    birdY = goalY;
+                }
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
         }
-
-        return super.onTouchEvent(event);
     }
 }
